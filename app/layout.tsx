b633d7181c17ea46b66c446b1a8707c2e20d6c5a
rohit <rohitsb96134@gmail.com>
2024-11-06 12:45:48 +0530

    Add Header component and integrate DaisyUI plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/Header/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content ">
+            <p>Copyright @ 2024 </p>
+          </footer>
+        </div>
       </body>
     </html>
   );
